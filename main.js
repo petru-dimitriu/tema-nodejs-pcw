47fa45bf1ebbf04ee66caf70ba99c6edb7f0022c
Petru Dimitriu <dimitriupetru@gmail.com>
2017-12-14 11:47:11 +0200

    Login, game start

--- main.js
+++ main.js
@@ -1,16 +1,100 @@
 var express = require('express');
 var app = express();
 
+
+var gameStatus = "WAITING";
+var loggedOnPlayers = 0;
+
 var server = app.listen(8082, function ()
 {
     console.log('Serverul ruleaza la portul 8082');
 });
 
 var io = require('socket.io')(server);
+var clientList = []; 
 
 io.on('connection', function (client)
 {
     console.log('S-a conectat un client!!!');
 
-    client.emit('test','Bun venit');
+    if (gameStatus != "WAITING")
+    {
+        client.emit('busy');
+        return;
+    }
+    client.emit('welcome','');
+    clientList.push(client);
+    broadcastStatus();
+
+    // elimina clientul din lista cand se conecteaza
+    client.on('disconnect', function ()
+    {
+        console.log('S-a deconectat un client!');
+        indexClient = clientList.indexOf(client);
+        // clientul avea setat numele player-ului deci era logat => scadem numarul de player-i logati
+        if (client.playerName !== null)
+        {
+            loggedOnPlayers--;
+            if (loggedOnPlayers < 0)
+                loggedOnPlayers = 0;
+        }
+        clientList.splice(indexClient,1);
+    });
+
+    client.on('enter', function (name)
+    {
+        console.log(name + ' has entered. ');
+        client.playerName = name;
+        loggedOnPlayers++;
+        client.emit('entered');
+        if (loggedOnPlayers != 0 && loggedOnPlayers == clientList.length)
+        {
+            broadcastReady();
+            setTimeout(broadcastStartGame,3000);
+        }
+    });
+
+    setInterval(broadcastStatus, 3000);
 });
+
+// trimite catre toti clientii starea curenta a jocului
+
+function broadcastStatus()
+{
+    var playerNameList = [];
+
+    for (var i = 0; i < clientList.length ; i++)
+    {
+        if (clientList[i].playerName !== undefined)
+            playerNameList.push(clientList[i].playerName);
+    }
+    
+    var status = 
+    { "status" : gameStatus,
+      "connectedPlayers" : clientList.length,
+      "loggedOnPlayers" : loggedOnPlayers,
+      "playerNameList" : playerNameList
+    };
+
+    for (var i = 0 ; i < clientList.length; i ++) {
+        clientList[i].emit('status',status);
+    }
+}
+
+function broadcastReady()
+{
+    for (var i = 0; i < clientList.length ; i++)
+    {
+        clientList[i].emit('ready');
+    }
+}
+
+function broadcastStartGame()
+{
+    var text = "Lorem ipsum dolor";
+    gameStatus = "PLAYING";
+    for (var i = 0; i < clientList.length ; i++)
+    {
+        clientList[i].emit('startGame',text);
+    }
+}
